b0221c6b09a1aa4382e21768caddf37e24f2550c
Niloy <iamvrniloyc@gmail.com>
2025-02-07 19:46:57 +0530

    Added Convex Database

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -2,22 +2,47 @@
 import { MessagesContext } from '@/context/MessagesContext'
 import { UserDetailContext } from '@/context/UserDetailContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ConvexClientProvider from './ConvexClientProvider';
+import { useConvex } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 const provider = ({children}) => {
   const [messages,setMessages]=useState();
   const [userDetail,setUserDetail]=useState();
+
+  const convex = useConvex();
+  useEffect(() => {
+    isAuthenticated();
+  },[])
+
+  const isAuthenticated=async()=>{
+    if(typeof window !== 'undefined'){
+      const user=JSON.parse(localStorage.getItem('user'));
+      if(!user){
+        return;
+      }
+      const result = await convex.query(api.users.GetUser,{email:user?.email});
+      if(!result){
+        return;
+      }
+      setUserDetail(result);
+      console.log(result)
+      //fetch user from db
+
+    }
+  }
+
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_AUTH_KEY}>
-      <ConvexClientProvider>
+      {/* <ConvexClientProvider> */}
 
     <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
     <MessagesContext.Provider value={{messages,setMessages}}>
     <div>{children}</div>
     </MessagesContext.Provider>
     </UserDetailContext.Provider>
-      </ConvexClientProvider>
+      {/* </ConvexClientProvider> */}
     </GoogleOAuthProvider>
   )
 }

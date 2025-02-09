77367dafa880e550561ed86bb1d28af326352b9b
Niloy <iamvrniloyc@gmail.com>
2025-02-09 23:24:57 +0530

    Backend

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
           <Provider>
         {children}
         </Provider>
-             </ConvexClientProvider>
+             </ConvexClientProvider> 
       </body>
     </html>
   );
